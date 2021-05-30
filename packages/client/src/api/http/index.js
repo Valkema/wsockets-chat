0fa0318e5e4a2bf0776b5ffe9e1941e6f4a4c537
Valkema <usagiserenity16@gmail.com>
2021-05-30 11:29:58 +0300

    sockets io created

---
+++ packages/client/src/api/http/index.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+const http = axios({
+    baseURL: 'http://localhost:3000/api',
+});
+
+export default http;
---
+++ packages/server/ws/eventsHandlers/index.js
@@ -0,0 +1,16 @@
+module.exports.testHandler = (data, options) => {
+    console.log('data = ', data);
+    console.log('options:', options);
+};
+
+/*
+module.exports.messageHandler = (data, options) => {
+    console.log('data = ', data);
+    console.log('options:', options);
+};
+
+module.exports.messageHandler = (room, message) => {
+    console.log('room = ', room);
+    console.log('message:', message);
+};
+*/
